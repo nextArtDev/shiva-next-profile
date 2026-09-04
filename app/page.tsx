dea8a445e607de7127dee9532c82f44142ff6a6f
nextArtDev <nextartisticdevelopments@gmail.com>
2026-09-04 20:22:00 +0330

    sync: local changes Sep 2026

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Hero from '@/components/Hero'
 import Booking from '@/components/booking'
 import FeaturedIn from '@/components/FeaturedIn'
@@ -7,7 +6,7 @@ import Gallery from '@/components/Gallery'
 import Card from '@/components/Card'
 import Diseases from '@/components/diseases/Diseases'
 import NewBooking from '@/components/NewBooking'
-=======
+
 import Image from 'next/image'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
@@ -20,7 +19,6 @@ import Card from './components/Card'
 import Diseases from './components/diseases/Diseases'
 import { LogoTicker } from './components/logo-tricker/LogoTicker'
 import Places from './components/Places'
->>>>>>> d5b4bd7c27538e4ec0175902755af3dc0fc8bff5
 
 export default function Home() {
   return (
@@ -30,25 +28,22 @@ export default function Home() {
       bg-page-gradient text-primary-text "
       >
         <Hero />
-<<<<<<< HEAD
-=======
+
         <LogoTicker />
         <Places />
         <Diseases />
->>>>>>> d5b4bd7c27538e4ec0175902755af3dc0fc8bff5
+
         <Booking />
         <Diseases />
 
         <Gallery />
-<<<<<<< HEAD
 
         <FeaturedIn />
         <NewBooking />
-=======
+
         {/* <Projects /> */}
         {/* <Card /> */}
         {/* <FeaturedIn /> */}
->>>>>>> d5b4bd7c27538e4ec0175902755af3dc0fc8bff5
       </main>
     </>
   )
